12f160ca63526002ecc7a928051c9f32702a26d3
Martin Stewart <MartinSStewart@gmail.com>
2021-08-18 21:07:13 +0200

    Add Process.sleep and improve Task module

--- src/Effect/Task.elm
+++ src/Effect/Task.elm
@@ -1,19 +1,35 @@
 module Effect.Task exposing
-    ( Task
-    , andThen
-    , fail
-    , map
-    , map2
-    , map3
-    , map4
-    , map5
-    , mapError
-    , onError
-    , succeed
-    , wait
+    ( Task, perform, attempt
+    , andThen, succeed, fail, sequence
+    , map, map2, map3, map4, map5
+    , onError, mapError
     )
 
-import Duration exposing (Duration)
+{-| Tasks make it easy to describe asynchronous operations that may fail, like
+HTTP requests or writing to a database.
+
+
+# Tasks
+
+@docs Task, perform, attempt
+
+
+# Chains
+
+@docs andThen, succeed, fail, sequence
+
+
+# Maps
+
+@docs map, map2, map3, map4, map5
+
+
+# Errors
+
+@docs onError, mapError
+
+-}
+
 import Effect.Internal exposing (Effect(..), HttpBody(..), Task(..))
 
 
@@ -29,11 +45,58 @@ type alias Effect restriction toMsg msg =
     Effect.Internal.Effect restriction toMsg msg
 
 
+{-| Here are some common tasks:
+
+  - [`now : Task x Posix`][now]
+  - [`focus : String -> Task Error ()`][focus]
+  - [`sleep : Float -> Task x ()`][sleep]
+
+[now]: /packages/elm/time/latest/Time#now
+[focus]: /packages/elm/browser/latest/Browser-Dom#focus
+[sleep]: /packages/elm/core/latest/Process#sleep
+
+In each case we have a `Task` that will resolve successfully with an `a` value
+or unsuccessfully with an `x` value. So `Browser.Dom.focus` we may fail with an
+`Error` if the given ID does not exist. Whereas `Time.now` never fails so
+I cannot be more specific than `x`. No such value will ever exist! Instead it
+always succeeds with the current POSIX time.
+
+More generally a task is a _description_ of what you need to do. Like a todo
+list. Or like a grocery list. Or like GitHub issues. So saying "the task is
+to tell me the current POSIX time" does not complete the task! You need
+[`perform`](#perform) tasks or [`attempt`](#attempt) tasks.
+
+-}
 type alias Task restriction x a =
     Effect.Internal.Task restriction x a
 
 
-{-| -}
+{-| Like I was saying in the [`Task`](#Task) documentation, just having a
+`Task` does not mean it is done. We must command Elm to `perform` the task:
+
+    -- elm install elm/time
+
+
+    import Task
+    import Time
+
+    type Msg
+        = Click
+        | Search String
+        | NewTime Time.Posix
+
+    getNewTime : Cmd Msg
+    getNewTime =
+        Task.perform NewTime Time.now
+
+If you have worked through [`guide.elm-lang.org`][guide] (highly recommended!)
+you will recognize `Cmd` from the section on The Elm Architecture. So we have
+changed a task like "make delicious lasagna" into a command like "Hey Elm, make
+delicious lasagna and give it to my `update` function as a `Msg` value."
+
+[guide]: https://guide.elm-lang.org/
+
+-}
 perform : (a -> msg) -> Task restriction Never a -> Effect restriction toMsg msg
 perform f task =
     task
@@ -43,6 +106,32 @@ perform f task =
 
 
 {-| This is very similar to [`perform`](#perform) except it can handle failures!
+So we could _attempt_ to focus on a certain DOM node like this:
+
+    -- elm install elm/browser
+
+
+    import Browser.Dom
+    import Task
+
+    type Msg
+        = Click
+        | Search String
+        | Focus (Result Browser.DomError ())
+
+    focus : Cmd Msg
+    focus =
+        Task.attempt Focus (Browser.Dom.focus "my-app-search-box")
+
+So the task is "focus on this DOM node" and we are turning it into the command
+"Hey Elm, attempt to focus on this DOM node and give me a `Msg` about whether
+you succeeded or failed."
+
+**Note:** Definitely work through [`guide.elm-lang.org`][guide] to get a
+feeling for how commands fit into The Elm Architecture.
+
+[guide]: https://guide.elm-lang.org/
+
 -}
 attempt : (Result x a -> msg) -> Task restriction x a -> Effect restriction toMsg msg
 attempt f task =
@@ -52,12 +141,24 @@ attempt f task =
         |> Task
 
 
-wait : Duration -> Task restriction x ()
-wait duration =
-    SleepTask duration Succeed
+{-| Chain together a task and a callback. The first task will run, and if it is
+successful, you give the result to the callback resulting in another task. This
+task then gets run. We could use this to make a task that resolves an hour from
+now:
+
+    -- elm install elm/time
 
 
-{-| Chain together a task and a callback.
+    import Process
+    import Time
+
+    timeInOneHour : Task x Time.Posix
+    timeInOneHour =
+        Process.sleep (60 * 60 * 1000)
+            |> andThen (\_ -> Time.now)
+
+First the process sleeps for an hour **and then** it tells us what time it is.
+
 -}
 andThen : (a -> Task restriction x b) -> Task restriction x a -> Task restriction x b
 andThen f task =
@@ -121,28 +222,82 @@ andThen f task =
             FileToUrl file (function >> andThen f)
 
 
-{-| A task that succeeds immediately when run.
+{-| A task that succeeds immediately when run. It is usually used with
+[`andThen`](#andThen). You can use it like `map` if you want:
+
+    import Time
+
+
+    -- elm install elm/time
+    timeInMillis : Task x Int
+    timeInMillis =
+        Time.now
+            |> andThen (\t -> succeed (Time.posixToMillis t))
+
 -}
 succeed : a -> Task restriction x a
 succeed =
     Succeed
 
 
-{-| A task that fails immediately when run.
+{-| A task that fails immediately when run. Like with `succeed`, this can be
+used with `andThen` to check on the outcome of another task.
+
+    type Error
+        = NotFound
+
+    notFound : Task Error a
+    notFound =
+        fail NotFound
+
 -}
 fail : x -> Task restriction x a
 fail =
     Fail
 
 
-{-| Transform a task.
+{-| Transform a task. Maybe you want to use [`elm/time`][time] to figure
+out what time it will be in one hour:
+
+    import Task exposing (Task)
+    import Time
+
+
+    -- elm install elm/time
+    timeInOneHour : Task x Time.Posix
+    timeInOneHour =
+        Task.map addAnHour Time.now
+
+    addAnHour : Time.Posix -> Time.Posix
+    addAnHour time =
+        Time.millisToPosix (Time.posixToMillis time + 60 * 60 * 1000)
+
+[time]: /packages/elm/time/latest/
+
 -}
 map : (a -> b) -> Task restriction x a -> Task restriction x b
 map f =
     andThen (f >> Succeed)
 
 
-{-| Put the results of two tasks together.
+{-| Put the results of two tasks together. For example, if we wanted to know
+the current month, we could use [`elm/time`][time] to ask:
+
+    import Task exposing (Task)
+    import Time
+
+
+    -- elm install elm/time
+    getMonth : Task x Int
+    getMonth =
+        Task.map2 Time.toMonth Time.here Time.now
+
+**Note:** Say we were doing HTTP requests instead. `map2` does each task in
+order, so it would try the first request and only continue after it succeeds.
+If it fails, the whole thing fails!
+
+[time]: /packages/elm/time/latest/
+
 -}
 map2 : (a -> b -> result) -> Task restriction x a -> Task restriction x b -> Task restriction x result
 map2 func taskA taskB =
@@ -154,8 +309,7 @@ map2 func taskA taskB =
             )
 
 
-{-| Put the results of three tasks together.
--}
+{-| -}
 map3 : (a -> b -> c -> result) -> Task restriction x a -> Task restriction x b -> Task restriction x c -> Task restriction x result
 map3 func taskA taskB taskC =
     taskA
@@ -170,8 +324,7 @@ map3 func taskA taskB taskC =
             )
 
 
-{-| Put the results of four tasks together.
--}
+{-| -}
 map4 :
     (a -> b -> c -> d -> result)
     -> Task restriction x a
@@ -196,8 +349,7 @@ map4 func taskA taskB taskC taskD =
             )
 
 
-{-| Put the results of five tasks together.
--}
+{-| -}
 map5 :
     (a -> b -> c -> d -> e -> result)
     -> Task restriction x a
@@ -227,7 +379,20 @@ map5 func taskA taskB taskC taskD taskE =
             )
 
 
-{-| Transform the error value.
+{-| Transform the error value. This can be useful if you need a bunch of error
+types to match up.
+
+    type Error
+        = Http Http.Error
+        | WebGL WebGL.Error
+
+    getResources : Task Error Resource
+    getResources =
+        sequence
+            [ mapError Http serverTask
+            , mapError WebGL textureTask
+            ]
+
 -}
 mapError : (x -> y) -> Task restriction x a -> Task restriction y a
 mapError f task =
@@ -291,7 +456,29 @@ mapError f task =
             FileToUrl file (function >> mapError f)
 
 
-{-| Recover from a failure in a task.
+{-| Start with a list of tasks, and turn them into a single task that returns a
+list. The tasks will be run in order one-by-one and if any task fails the whole
+sequence fails.
+
+    sequence [ succeed 1, succeed 2 ] == succeed [ 1, 2 ]
+
+-}
+sequence : List (Task restriction x a) -> Task restriction x (List a)
+sequence tasks =
+    List.foldr (map2 (::)) (succeed []) tasks
+
+
+{-| Recover from a failure in a task. If the given task fails, we use the
+callback to recover.
+
+    fail "file not found"
+      |> onError (\msg -> succeed 42)
+      -- succeed 42
+
+    succeed 9
+      |> onError (\msg -> succeed 42)
+      -- succeed 9
+
 -}
 onError : (x -> Task restriction y a) -> Task restriction x a -> Task restriction y a
 onError f task =
