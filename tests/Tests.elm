acec62f5a5ed0bf6ed12d16287508c4f08c32f46
Martin Stewart <MartinSStewart@gmail.com>
2024-12-17 15:36:20 +0100

    SSCCE

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,59 +1,7 @@
-module Tests exposing (..)
+module Tests exposing (main)
 
-import Effect.Command as Command exposing (BackendOnly, Command, FrontendOnly)
-import Effect.Http exposing (Response(..))
-import Effect.Subscription as Subscription
-import Effect.Test exposing (FileUpload(..), HttpResponse(..), MultipleFilesUpload(..))
-import Html
-import Url
+import Effect.Test
 
 
-frontendApp : Effect.Test.FrontendApp {} {} {} {}
-frontendApp =
-    { init = \_ _ -> ( {}, Command.none )
-    , onUrlRequest = \_ -> {}
-    , onUrlChange = \_ -> {}
-    , update = \_ _ -> ( {}, Command.none )
-    , updateFromBackend = \_ _ -> ( {}, Command.none )
-    , view = \_ -> { title = "Hi", body = [ Html.text "Hi" ] }
-    , subscriptions = \_ -> Subscription.none
-    }
-
-
-backendApp : Effect.Test.BackendApp {} {} {} {}
-backendApp =
-    { init = ( {}, Command.none )
-    , update = \_ _ -> ( {}, Command.none )
-    , updateFromFrontend = \_ _ _ _ -> ( {}, Command.none )
-    , subscriptions = \_ -> Subscription.none
-    }
-
-
-unsafeUrl =
-    case Url.fromString "my-test.app" of
-        Just url ->
-            url
-
-        Nothing ->
-            Debug.todo "Invalid url"
-
-
-config : Effect.Test.Config {} {} {} {} {} {}
-config =
-    { frontendApp = frontendApp
-    , backendApp = backendApp
-    , handleHttpRequest = always NetworkErrorResponse
-    , handlePortToJs = always Nothing
-    , handleFileUpload = always CancelFileUpload
-    , handleMultipleFilesUpload = always CancelMultipleFilesUpload
-    , domain = unsafeUrl
-    }
-
-
-test =
-    Effect.Test.start config "A test"
-
-
-main : Program () (Effect.Test.Model {} {} {} {} {} {}) (Effect.Test.Msg {} {} {} {} {} {})
 main =
-    Effect.Test.viewer [ test ]
+    Effect.Test.viewer []
